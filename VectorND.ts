0b87ee5aa555529f89b213bc1a32651aabf9c90b
Michael James <micrjames921@gmail.com>
2024-12-29 21:51:18 -0700

    wrote methods to setup and look at values of the 'vector'; completed tests for the basic setup of an N-dimensional vector

--- VectorND.ts
+++ VectorND.ts
@@ -1,23 +1,48 @@
 import { StringBuilder } from "./StringBuilder/StringBuilder";
+import { Utilities } from "./Utilities/Utilities";
 
 export class VectorND {
-   private _vec: number[][];
-   private N: number;
+   private _vec: number[];
+   private _N: number;
+   private utility: Utilities;
 
    constructor(N: number) {
-	  this.N = N;
-	  this.clear();
+	  this._N = N;
+	  this.reset();
+
+	  this.utility = new Utilities();
    }
 
    private set() {
 	  this._vec = Array(this.N).fill(0);
    }
 
-   clear() {
+   at(index: number): number {
+	  return this._vec[index];
+   }
+   indexOf(element: number): number {
+	  return this._vec.indexOf(element);
+   }
+
+   reset() {
 	  this._vec = [];
 	  this.set();
    }
 
-   toString() {
+   get N(): number {
+	  return this._N;
+   }
+
+   toString(): String {
+	  let sb = new StringBuilder();
+	  sb.append('[');
+	  this.utility.iterate.iterate(this._vec, (el, idx) => {
+		 if(idx != 0) sb.appendSp();
+		 sb.append(`${el}`);
+		 if(idx != this.N-1) sb.appendComma();
+	  });
+	  sb.append(']');
+
+	  return sb.build();
    }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,17 +1,48 @@
 import { VectorND } from "../VectorND";
 
 describe("A Vector", () => {
-   let numEls: number;
+   let N: number;
    let vec2D: VectorND;
+   let vec2DSize: number;
    beforeAll(() => {
-	  numEls = 2;
-	  vec2D = new VectorND(2);
-	  console.log(vec2D);
+	  N = 2;
+	  vec2D = new VectorND(N);
+	  vec2DSize = vec2D.N;
    });
    describe("A Vector implemented initially with all zeroes.", () => {
-	  test.todo("Should not be undefined.");
-	  test.todo("Should have elements that are not undefined.");
-	  test.todo("Should consist of 4 elements.");
-	  test.todo("Should consist of 4 element set of all zeros.");
+	  test("Should not be undefined.", () => {
+		 expect(vec2D).toBeDefined();
+	  });
+	  test("Should have elements that are not undefined.", () => {
+		 for(const idx of [0, 1]) {
+			expect(vec2D.at(idx)).toBeDefined();
+		 }
+	  });
+	  test("Should consist of 2 elements.", () => {
+		 expect(vec2DSize).toBe(N);
+	  });
+	  test("Should have each element typed as number.", () => {
+		 for(const idx of [0, 1]) {
+			expect(vec2D.at(idx)).toEqual(expect.any(Number));
+		 }
+	  });
+	  test("Should have a 'zero' as the first element.", () => {
+		 let elementIdx: number;
+		 const element = elementIdx = 0;
+		 const isZero1stElement = vec2D.indexOf(element) === elementIdx;
+		 expect(isZero1stElement).toBeTruthy();
+	  });
+	  test("Should consist of a two element set of all zeros.", () => {
+		 const element = 0;
+		 let vec2DArr: number[] = [];
+		 for(const idx of [0, 1]) {
+			if(vec2D.at(idx) === element) vec2DArr.push(element);
+		 }
+		 expect(vec2DArr).toHaveLength(vec2DSize);
+	  });
    });
 });
+
+/*
+   indexOf(element: number): number
+*/
